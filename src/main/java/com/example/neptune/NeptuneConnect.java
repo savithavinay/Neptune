a62a48a20a8d7dd6f6702dd34db1f44e5e764e90
savithavinay <savithavinaykrishnan@gmail.com>
2022-07-22 22:55:52 +1000

    Update NeptuneConnect.java

--- src/main/java/com/example/neptune/NeptuneConnect.java
+++ src/main/java/com/example/neptune/NeptuneConnect.java
@@ -1,8 +1,10 @@
 package com.example.neptune;
 
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.neptune.auth.NeptuneNettyHttpSigV4Signer;
+import com.amazonaws.neptune.auth.NeptuneSigV4SignerException;
 import org.apache.tinkerpop.gremlin.driver.Client;
 import org.apache.tinkerpop.gremlin.driver.Cluster;
-import org.apache.tinkerpop.gremlin.driver.SigV4WebSocketChannelizer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
@@ -13,10 +15,20 @@ public class NeptuneConnect {
         System.out.println("*****");
 
         Cluster cluster = Cluster.build("demo.cluster-custom-chuoniryb3ms.ap-southeast-2.neptune.amazonaws.com")
-                .port(8182)
                 .enableSsl(true)
-                .channelizer(SigV4WebSocketChannelizer.class)
-                .create();
+                .handshakeInterceptor( r ->
+                        {
+                            NeptuneNettyHttpSigV4Signer sigV4Signer = null;
+                            try {
+                                sigV4Signer = new NeptuneNettyHttpSigV4Signer("ap-southeast-2",
+                                        new DefaultAWSCredentialsProviderChain());
+                                sigV4Signer.signRequest(r);
+                            } catch (NeptuneSigV4SignerException e) {
+                                e.printStackTrace();
+                            }
+                            return r;
+                        }
+                ).create();
         Client client = cluster.connect();
         System.out.println(client.submit("g.V().has('code','IAD')").all());
     }
