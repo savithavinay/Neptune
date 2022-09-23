2ecd4c1657413cb3218ac51843143adf0d9b370d
savithavinay <savithavinaykrishnan@gmail.com>
2022-09-23 22:39:33 +1000

    Update NeptuneApplication.java

--- src/main/java/com/example/neptune/NeptuneApplication.java
+++ src/main/java/com/example/neptune/NeptuneApplication.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
